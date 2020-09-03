5036db238c1af6a9a61807f1a3befff242838cb8
Joon Lee <joon.lee727@gmail.com>
2020-09-03 17:27:30 -0400

    Switch to Using Common Library
    Remove code in Auth that will be used across all of our services into
    its own NPM library (@ej-tickets/common).
    
    Import library and use it.

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@ej-tickets/common';
 
 const router = express.Router();
 
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -3,8 +3,7 @@ import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { BadRequestError, validateRequest } from '@ej-tickets/common';
 import { Password } from '../utils/password';
 
 const router = express.Router();
