7f8189ae5ec43a3f52187147bbfb811660b7c69b
Joon Lee <joon.lee727@gmail.com>
2020-12-30 11:57:58 -0500

    Basic Frontend Implementation
    - Adds pages for creating and viewing tickets
    - Adds Stripe Checkout purchase dialogue box
    - Adds page for viewing orders
    
    - The client is missing many features:
        - Any functionality relating to viewing purchased tickets
        - Better styling
        - Ability to refund/cancel a purchase
        - Pretty much most features that a e-commerce application should
        have

--- orders/src/events/listeners/payment-created-listener.ts
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -13,7 +13,7 @@ export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
     queueGroupName = queueGroupName;
 
     async onMessage(data: PaymentCreatedEvent['data'], msg: Message) {
-        const order = await Order.findById({ id: data.orderId });
+        const order = await Order.findById(data.orderId);
 
         if (!order) {
             throw new Error('Order not found');
